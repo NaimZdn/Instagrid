a53c9ef82826edf1edb859d7f11c997b8c104c1a
Namsco <naim.zidouni@outlook.fr>
2022-12-12 17:35:08 +0100

    Added an extension of UIView to transform UIView to UIImage for the activityController inside the ViewController.

---
+++ Instagrid/UIView.swift
@@ -0,0 +1,21 @@
+//
+//  UIView.swift
+//  Instagrid
+//
+//  Created by Zidouni on 12/12/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
