97b54d8983633a49cac1260514e34d963285750a
Namsco <naim.zidouni@outlook.fr>
2022-12-16 14:08:29 +0100

    Changed the UIImage to a UIImage optional

--- Instagrid/UIView.swift
+++ Instagrid/UIView.swift
@@ -12,7 +12,7 @@ extension UIView {
 
     // Using a function since `var image` might conflict with an existing variable
     // (like on `UIImageView`)
-    func asImage() -> UIImage {
+    func asImage() -> UIImage? {
         let renderer = UIGraphicsImageRenderer(bounds: bounds)
         return renderer.image { rendererContext in
             layer.render(in: rendererContext.cgContext)
