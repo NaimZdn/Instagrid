1a93bc8d57ff2983bea41e8b9308b8be736c2181
Namsco <naim.zidouni@outlook.fr>
2022-12-16 20:26:30 +0100

    Changed function's order.

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var layout3Selected: UIImageView!
     
     @IBOutlet weak var gridCentralView: UIView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,32 +51,116 @@ class ViewController: UIViewController {
         
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
+    func checkLibraryAuthorization() -> Bool {
+        var status = false
+        
+        let libraryAuthorization = PHPhotoLibrary.authorizationStatus()
+        switch libraryAuthorization {
+        case .authorized:
+            status = true
+            
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({
+                (newStatus) in
+                if newStatus == PHAuthorizationStatus.authorized {
+                    status = true
+                }
+            })
+            
+        case .restricted, .denied:
+            break
+            
+        case .limited:
+            print("Access Limited")
+            
+        @unknown default :
+            break
+        }
+        
+        return status
+    }
+    
+    @IBAction func layoutButtonsTapped(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            buttonLayout1.isSelected = true
+            buttonLayout2.isSelected = false
+            buttonLayout3.isSelected = false
+            
+            buttonUpRight.isHidden = true
+            buttonDownRight.isHidden = false
+            isSelected()
+            
+        case 2:
+            buttonLayout1.isSelected = false
+            buttonLayout2.isSelected = true
+            buttonLayout3.isSelected = false
+            
+            buttonUpRight.isHidden = false
+            buttonDownRight.isHidden = true
+            isSelected()
+            
+        case 3:
+            buttonLayout1.isSelected = false
+            buttonLayout2.isSelected = false
+            buttonLayout3.isSelected = true
+            
+            buttonUpRight.isHidden = false
+            buttonDownRight.isHidden = false
+            isSelected()
+        default:
+            break
+        }
+    }
+    
+    func isSelected() {
+        if buttonLayout1.isSelected == true {
+            layout1Selected.isHidden = false
+            layout2Selected.isHidden = true
+            layout3Selected.isHidden = true
+            
+        } else if buttonLayout2.isSelected == true {
+            layout1Selected.isHidden = true
+            layout2Selected.isHidden = false
+            layout3Selected.isHidden = true
+            
+        } else if buttonLayout3.isSelected == true {
+            layout1Selected.isHidden = true
+            layout2Selected.isHidden = true
+            layout3Selected.isHidden = false
+        }
+    }
+    
     func tapGesture() {
         let tapUpLeftButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
-            buttonUpLeft.isUserInteractionEnabled = true
-            buttonUpLeft.addGestureRecognizer(tapUpLeftButtonRecognizer)
+        buttonUpLeft.isUserInteractionEnabled = true
+        buttonUpLeft.addGestureRecognizer(tapUpLeftButtonRecognizer)
         
         
         let tapUpRightButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
-            buttonUpRight.isUserInteractionEnabled = true
-            buttonUpRight.addGestureRecognizer(tapUpRightButtonRecognizer)
+        buttonUpRight.isUserInteractionEnabled = true
+        buttonUpRight.addGestureRecognizer(tapUpRightButtonRecognizer)
         
-      
+        
         let tapDownLeftButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
-            buttonDownLeft.isUserInteractionEnabled = true
-            buttonDownLeft.addGestureRecognizer(tapDownLeftButtonRecognizer)
-    
- 
+        buttonDownLeft.isUserInteractionEnabled = true
+        buttonDownLeft.addGestureRecognizer(tapDownLeftButtonRecognizer)
+        
+        
         let tapDownRightButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
-            buttonDownRight.isUserInteractionEnabled = true
-            buttonDownRight.addGestureRecognizer(tapDownRightButtonRecognizer)
+        buttonDownRight.isUserInteractionEnabled = true
+        buttonDownRight.addGestureRecognizer(tapDownRightButtonRecognizer)
         
     }
     
     @objc func imageTapped(_ recognizer: UITapGestureRecognizer) {
-        var newImage = recognizer.view
-        imageTest = newImage as! UIImageView?
         if checkLibraryAuthorization() {
+            var newImage = recognizer.view
+            imageTest = newImage as! UIImageView?
             imagePicked = UIImagePickerController()
             imagePicked?.delegate = self
             imagePicked?.sourceType = .savedPhotosAlbum
@@ -90,9 +174,11 @@ class ViewController: UIViewController {
             self.present(accessDenied, animated: true, completion: nil)
         }
     }
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        .lightContent
+    
+    func insertPickedImageIntoMainGrid(_ image: UIImage) {
+        print(imageTest?.contentMode == .scaleAspectFill)
+        imageTest?.contentMode = .scaleAspectFill
+        imageTest?.image = image
     }
     
     func addSwipGestureRecognizer() {
@@ -101,14 +187,14 @@ class ViewController: UIViewController {
         
         let swipeLeftGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeGridCentralView(_:)))
         swipeLeftGestureRecognizer.direction = .left
-       
+        
         
         view.addGestureRecognizer(swipeUpGestureRecognizer)
         view.addGestureRecognizer(swipeLeftGestureRecognizer)
     }
     
     @objc func swipeGridCentralView(_ recognizer: UISwipeGestureRecognizer) {
- 
+        
         if UIDevice.current.orientation.isPortrait, recognizer.direction == .up{
             self.animationPortrait()
             shareTheLayout(direction: .up)
@@ -151,90 +237,6 @@ class ViewController: UIViewController {
         }
         present(activityVC, animated: true, completion: nil)
         
-    }
-
-    func insertPickedImageIntoMainGrid(_ image: UIImage) {
-        print(imageTest?.contentMode == .scaleAspectFill)
-        imageTest?.contentMode = .scaleAspectFill
-        imageTest?.image = image
-    }
-
-    @IBAction func layoutButtonsTapped(_ sender: UIButton) {
-        switch sender.tag {
-        case 1:
-            buttonLayout1.isSelected = true
-            buttonLayout2.isSelected = false
-            buttonLayout3.isSelected = false
-            
-            buttonUpRight.isHidden = true
-            buttonDownRight.isHidden = false
-            isSelected()
-        
-        case 2:
-            buttonLayout1.isSelected = false
-            buttonLayout2.isSelected = true
-            buttonLayout3.isSelected = false
-            
-            buttonUpRight.isHidden = false
-            buttonDownRight.isHidden = true
-            isSelected()
-            
-        case 3:
-            buttonLayout1.isSelected = false
-            buttonLayout2.isSelected = false
-            buttonLayout3.isSelected = true
-            
-            buttonUpRight.isHidden = false
-            buttonDownRight.isHidden = false
-            isSelected()
-        default:
-            break
-        }
-    }
-    
-    func isSelected() {
-        if buttonLayout1.isSelected == true {
-            layout1Selected.isHidden = false
-            layout2Selected.isHidden = true
-            layout3Selected.isHidden = true
-        } else if buttonLayout2.isSelected == true {
-            layout1Selected.isHidden = true
-            layout2Selected.isHidden = false
-            layout3Selected.isHidden = true
-        } else if buttonLayout3.isSelected == true {
-            layout1Selected.isHidden = true
-            layout2Selected.isHidden = true
-            layout3Selected.isHidden = false
-        }
-    }
-    
-    func checkLibraryAuthorization() -> Bool {
-        var status = false
-        
-        let libraryAuthorization = PHPhotoLibrary.authorizationStatus()
-        switch libraryAuthorization {
-        case .authorized:
-            status = true
-        
-        case .notDetermined:
-            PHPhotoLibrary.requestAuthorization({
-                (newStatus) in
-                if newStatus == PHAuthorizationStatus.authorized {
-                    status = true
-                }
-            })
-            
-        case .restricted, .denied:
-            break
-            
-        case .limited:
-            print("Access Limited")
-            
-        @unknown default :
-            break
-        }
-       
-        return status
     }
 }
 
